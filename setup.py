20bad89832370c5a3257dbb0c5a51942c9173bdc
Ronald <ronald@orderbird.com>
2013-11-15 23:01:59 +0100

    Added setup for test.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
  # -*- coding: utf-8 -*-
 
+import sys
 from setuptools import setup, find_packages
+try:
+    import multiprocessing
+except ImportError:
+    pass
 
 
 LONG_DESCRIPTION = None
@@ -16,16 +21,19 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Framework :: Django',
     'Programming Language :: Python',
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.5",
-    "Programming Language :: Python :: 2.6",
-    "Programming Language :: Python :: 2.7",
-    #"Programming Language :: Python :: 3",
-    #"Programming Language :: Python :: 3.1",
-    #"Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: Implementation :: CPython",
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    #'Programming Language :: Python :: 3',
+    #'Programming Language :: Python :: 3.1',
+    #'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
+
+extra_opts = {}
+if "test" in sys.argv or "nosetests" in sys.argv:
+    extra_opts['tests_require'] = ['nose', 'coverage', 'blinker']#, 'django>=1.4.2',]
 
 basic_setup = dict(
     name='aserializer',
@@ -36,9 +44,10 @@ basic_setup = dict(
     description='A serializer for rest.',
     url='',
     long_description=LONG_DESCRIPTION,
-    packages= find_packages(exclude=('tests',)),
+    packages= find_packages(exclude=('tests', 'tests.*')),
     platforms=['any'],
     classifiers=CLASSIFIERS,
     test_suite='nose.collector',
+    **extra_opts
 )
 setup(**basic_setup)
